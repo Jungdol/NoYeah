c24359421c1c4ca3df6053a15412800e871a30b3
dunft <j01026708401@gmail.com>
2021-12-19 07:44:40 +0900

    Ending

--- Assets/Scripts/Ending.cs
+++ Assets/Scripts/Ending.cs
@@ -12,42 +12,59 @@ public class Ending : MonoBehaviour
     public Text score;
     public Text Rank;
     public GameManager gameManager;
+    public GameObject Sucess;
+    public GameObject Fail;
     // Start is called before the first frame update
     void Start()
     {
         end.SetActive(false);
+        Sucess.SetActive(false);
+        Fail.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        scored = gameManager.elf * 1000;
+        scored = gameManager.elf * 375;
         elfCount.text =""+gameManager.elf;
         change.text = "" + gameManager.ChangeCnt;
         score.text = "" + scored ;
-        if (5000 <= scored&&scored<6000)
+        if (scored<6000)
         {
             Rank.text = "D";
+            Sucess.SetActive(false);
+            Fail.SetActive(true);
+
         }
         else if (6000 <= scored && scored < 7000)
         {
             Rank.text = "c";
+            Sucess.SetActive(false);
+            Fail.SetActive(true);
         }
         else if (7000 <= scored && scored < 8000)
         {
             Rank.text = "B";
+            Sucess.SetActive(true);
+            Fail.SetActive(false);
         }
         else if (8000 <= scored && scored < 9000)
         {
             Rank.text = "A";
+            Sucess.SetActive(true);
+            Fail.SetActive(false);
         }
         else if (9000 <= scored && scored < 10000)
         {
             Rank.text = "S";
+            Sucess.SetActive(true);
+            Fail.SetActive(false);
         }
         else if (10000 <= scored )
         {
             Rank.text = "S+";
+            Sucess.SetActive(true);
+            Fail.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
